afebcc5a228bbbc13505fbad67dad706e3151cfa
vojtechsoban <vojtech.soban@gmail.com>
2017-07-02 22:43:42 +0200

    wip

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,49 +1,97 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    'react-hot-loader/patch',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new HtmlWebpackPlugin({
-          title: 'Custom template using Handlebars',
-          template: 'src/index.hbs'
-    })
-  ],
+const defaultConfig = {
   resolve: {
     alias: {
-      'redux-devtools/lib': path.join(__dirname, '..', '..', 'src'),
-      'redux-devtools': path.join(__dirname, '..', '..', 'src'),
+      // 'redux-devtools/lib': path.join(__dirname, '..', '..', 'src'),
+      // 'redux-devtools': path.join(__dirname, '..', '..', 'src'),
       'react': path.join(__dirname, 'node_modules', 'react')
     },
-    extensions: ['', '.js']
+    modules: [path.resolve('./src'), 'node_modules'],
+    extensions: ['.js', '.jsx', '.css', '.scss']
   },
-  resolveLoader: {
-    'fallback': path.join(__dirname, 'node_modules')
+  output: {
+    path: path.resolve(__dirname, 'www/dist/'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          {loader: 'babel-loader'}
+        ],
+        exclude: /node_modules/
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
   }
 };
+
+//noinspection JSUnresolvedFunction
+module.exports = Object.assign({}, defaultConfig, {
+  devtool: 'source-map',
+  devServer: {
+    host: 'localhost',
+    port: '3000',
+    
+    // match the output path
+    contentBase: path.resolve(__dirname, 'dist'),
+    
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    publicPath: '/'
+  },
+  
+  output: {
+    // the output bundle
+    filename: 'bundle.js',
+    
+    path: path.join(__dirname, 'dist'),
+    
+    // necessary for HMR to know where to load the hot update chunks
+    publicPath: '/'
+  },
+  
+  entry: [
+    // activate HMR for React
+    'react-hot-loader/patch',
+    
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+    'webpack-dev-server/client?http://localhost:3000',
+    
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+    'webpack/hot/only-dev-server',
+    
+    // 'babel-polyfill',
+    
+    // the entry point of our app
+    './src/index'
+  ],
+  
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Custom template using Handlebars',
+      template: 'src/index.hbs'
+    }),
+    new webpack.DefinePlugin({
+      __DEVELOPMENT__: JSON.stringify(true)
+    })
+  ]
+});
